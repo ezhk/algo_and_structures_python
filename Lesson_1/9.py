d9b683aff3867533909b28d3d73dac863ddf3291
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-07-30 19:19:14 +0300

    refactoring 9.py

--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -4,26 +4,56 @@
 # является средним (больше одного, но меньше другого).
 
 def search_middle_number(a, b, c):
-    min = a
-    if min > b:
-        min = b
-    if min > c:
-        min = c
+    def _find_min(a, b, c):
+        """
+        Function search minimum value from 3 params
+        :param a: integer or float value
+        :param b: integer or float value
+        :param c: integer or float value
+        :return minimum value: integer or float
+        """
+        min = a
+        if min > b:
+            min = b
+        if min > c:
+            min = c
+        return min
 
-    max = a
-    if max < b:
-        max = b
-    if max < c:
-        max = c
+    def _find_max(a, b, c):
+        """
+        Function search maximum value from 3 params
+        :param a: integer or float value
+        :param b: integer or float value
+        :param c: integer or float value
+        :return maximum value: integer or float
+        """
+        max = a
+        if max < b:
+            max = b
+        if max < c:
+            max = c
+        return max
 
+    def _is_middle(min, value, max):
+        """
+        Function check that middle argument less than max and more than min
+        :param min: integer or float value
+        :param value: integer or float value
+        :param max: integer or float value
+        :return result of expression: bool
+        """
+        return min < value < max
+
+    min = _find_min(a, b, c)
+    max = _find_max(a, b, c)
     print(f"\tМинимальное число: {min}")
     print(f"\tМаксмиальное число: {max}")
 
-    if max > a > min:
+    if _is_middle(min, a, max):
         return a
-    if max > b > min:
+    if _is_middle(min, b, max):
         return b
-    if max > c > min:
+    if _is_middle(min, c, max):
         return c
 
     return None
